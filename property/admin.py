773d09a3823bc4e4bdb629fa6e879d5fb31eb945
Vitaly Borodulin <rainbowf@mail.ru>
2022-05-18 21:57:10 +0500

    Add owners information on flat's page in admin

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.owner_flats.through
+    raw_id_fields = ['owner']
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'id']
     readonly_fields = ['created_at']
@@ -20,6 +25,7 @@ class FlatAdmin(admin.ModelAdmin):
         'has_balcony',
     ]
     raw_id_fields = ['likes']
+    inlines = [OwnerInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
